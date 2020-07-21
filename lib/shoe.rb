3890e189726abe66892d3b5e3e1e0e996799291f
Katie Le <lethikatie@gmail.com>
2020-07-21 02:56:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,15 +8,11 @@ class Shoe
     @brand = brand
     BRANDS << brand
   end
-  
-  unless size != 4
-    puts 3
-  else 
-    puts 4
-  end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  if size 
 end 
