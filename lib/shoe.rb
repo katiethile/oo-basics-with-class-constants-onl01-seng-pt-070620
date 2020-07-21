8f812678dff070e46a7f74febdb2bbc0f19af82d
Katie Le <lethikatie@gmail.com>
2020-07-21 02:28:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
   attr_accessor :color, :material, :condition
   attr_reader :brand, :size
-  
-  BRANDS = []
 
   def initialize(brand)
     @brand = brand
   end
+  
+  BRANDS = []
 
   def cobble
     self.condition = "new"
